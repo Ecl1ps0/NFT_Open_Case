ee5a835d171e766e9a02417df7f92edf228d8773
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-10-11 00:13:40 +0600

    feat: set up constractor, Item and getters

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -12,7 +12,49 @@ contract NFTOpenCase is ERC721URIStorage {
 
     using Counters for Counters.Counter;
 
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listPrice = 0.01 ether;
+
     constructor() ERC721("NFTOpenCase", "NFTOC") {
         owner = payable(msg.sender);
-    }  
+    }
+
+    mapping(uint256 => Item) private idItem;
+
+    modifier ownerOnly() {
+        require(owner == msg.sender, "Only owner can change the listing price!");
+        _;
+    }
+
+    struct Item {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool sold;
+    } 
+
+    function getCurrentToken() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function getListPrice() public view returns (uint256) {
+        return listPrice;
+    }
+
+    function updateListPrice(uint256 _listPrice) public payable ownerOnly {
+        listPrice = _listPrice;
+    }
+
+    function getLatestTokenIdByItem() public view returns (Item memory) {
+         return idItem[getCurrentToken()];
+    }
+
+    function getItemByTokenId(uint256 tokenId) public view returns (Item memory) {
+        return idItem[tokenId];
+    }
+
+    
 }
