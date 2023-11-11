2353d40b65dc3569c9bf9ebbf0b01f332ff1db61
Arystan <zhulumbetov.ar@gmail.com>
2023-11-12 03:33:27 +0600

    okay

---
+++ src/components/info.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './info.css';
+
+const Info = () => {
+  return (
+    <section className='info-section'>
+      <h2>Praise the Bocchi queen</h2>
+      <p>
+      Hitori is a true rock star whose music touches the deepest strings of the human soul. Her voice is not just a sound, but a source of inspiration for millions.
+      </p>
+      <p id="special-text">
+      I am Aldiyar Seylkhanov and I lower my knee to the true queen of all waifus
+      </p>
+    </section>
+  );
+};
+
+export default Info;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Case from '../components/Case';
+import Info from '../components/info';
 
 const Home = () => {
 	return (
 		<main className='p-12 container mx-auto font-rubik justify-center w-full flex flex-wrap gap-5'>
-			{new Array(20).fill(0).map((_) => {
+			{new Array(10).fill(0).map((_) => {
 				return <Case />;
 			})}
+			<Info />
 		</main>
 	);
 };
