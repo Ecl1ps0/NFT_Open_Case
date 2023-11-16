5267c9b5e92e1e31b869d596aec18b0f9098da5d
Arystan <zhulumbetov.ar@gmail.com>
2023-11-16 22:03:59 +0600

    title page update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,12 +4,21 @@ import back1 from '../assets/back1.png';
 import back2 from '../assets/back2.png';
 import back3 from '../assets/back3.png';
 import back4 from '../assets/back4.png';
-const imagesContext = require.context('../assets/collection', false, /\.(png)$/);
-const collectionImages = imagesContext.keys().map((imagePath) => {
-    const image = imagesContext(imagePath);
-    console.log(image); // Log the imported image
-    return image;
-  });
+
+import col1 from '../assets/collection/0001.png';
+import col2 from '../assets/collection/0002.png';
+import col3 from '../assets/collection/0003.png';
+import col4 from '../assets/collection/0004.png';
+import col5 from '../assets/collection/0005.png';
+import col6 from '../assets/collection/0006.png';
+import col7 from '../assets/collection/0007.png';
+import col8 from '../assets/collection/0008.png';
+import col9 from '../assets/collection/0009.png';
+import col10 from '../assets/collection/0010.png';
+import col11 from '../assets/collection/0011.png';
+import col12 from '../assets/collection/0012.png';
+const collectionImages = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12];
+
   
 
 export const Title = () => {
@@ -27,7 +36,7 @@ export const Title = () => {
         <h1 className="text-4xl font-bold mb-14 font-bold text-[#0f0f0f] text-[70px] mt-4">Bocchi the Case</h1>
         <p className="text-sm text-gray-500 font-medium text-[#0f0f0f] text-[25px] ">500 images from the Bocchi the Case</p>
         <div className="mt-20">
-          <button className="bg-green-500 text-white py-5 px-40 rounded [font-family:'Inter',Helvetica] font-medium text-[#0f0f0f] text-[18px] text-center">
+          <button onClick={()=>window.location.href='/'} className="bg-green-500 text-white py-5 px-40 rounded [font-family:'Inter',Helvetica] font-medium text-[#0f0f0f] text-[18px] text-center">
             Open the Case
           </button>
         </div>
@@ -58,16 +67,16 @@ export const Title = () => {
       <div className="flex-1 bg-gray-200 p-8">
         <h2 className="text-3xl font-bold text-center mb-4">Browse our collection</h2>
         
-        <div className="grid grid-cols-10 gap-4">
-  {collectionImages.map((image, index) => (
-    <img
-      key={index}
-      className="object-cover h-10 w-10 rounded"
-      src={image.default} 
-      alt={`Image ${index + 1}`}
-    />
-  ))}
-</div>
+        <div className="grid grid-cols-4 gap-4 mx-center">
+      {collectionImages.map((image, index) => (
+        <img
+          key={index}
+          className="object-cover w-[200px] h-[200px] rounded"
+          src={image}
+          alt={`Image ${index + 1}`}
+        />
+      ))}
+    </div>
         </div>
     </div>
   );
